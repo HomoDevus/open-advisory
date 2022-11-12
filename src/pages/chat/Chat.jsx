ff72222ff328b0c36804a494d1c003c3e30e5585
ItisEternity <lavrovslava207@gmail.com>
2022-11-12 21:14:40 +0300

    Chat scrolled to bottom by default

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './Chat.module.css';
 import Message from './components/Message/Message';
 import MessageInput from './components/MessageInput/MessageInput';
@@ -11,16 +11,28 @@ const Chat = () => {
   const dispatch = useDispatch()
   const messages = useSelector(selectMessages)
 
+  const chatContainer = useRef(null);
+
   // const {data, isFetching, isLoading} = useGetMessagesQuery()
 
   useEffect(() => {
     dispatch(getChatHistory())
   }, [dispatch])
 
+  useEffect(() => {
+    if (chatContainer.current) {
+      window.scrollTo(0, chatContainer.current.scrollHeight);
+
+    }
+  }, [messages])
+
   return (
-    <div className={styles.chat}>
+    <div className={styles.chat} ref={chatContainer}>
       <div className={styles.messagesContainer}>
-        {messages?.slice(0).reverse().map(message => <div key={message.messageId}><UserBadge userName={message.sender === 100112 ? 'Брокер' : 'Клиент'} /><Message isReceived={message.sender !== 100112} timestamp={message.timestamp} text={message.text} /></div>)}
+        {messages?.slice(0).reverse().map(message => <div key={message.messageId}><UserBadge
+          userName={message.sender === 100112 ? 'Брокер' : 'Клиент'} /><Message isReceived={message.sender !== 100112}
+                                                                                timestamp={message.timestamp}
+                                                                                text={message.text} /></div>)}
       </div>
       <MessageInput />
     </div>
