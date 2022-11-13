fd330d690c77e694e97b836d56f8385baeaf9e3e
ItisEternity <lavrovslava207@gmail.com>
2022-11-13 11:54:23 +0300

    Readme change

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import chatReducer from '../features/chat/chatSlice';
 import { messagesWs } from '../features/chat/chatApi';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     chat: chatReducer,
     [messagesWs.reducerPath]: messagesWs.reducer
   },
